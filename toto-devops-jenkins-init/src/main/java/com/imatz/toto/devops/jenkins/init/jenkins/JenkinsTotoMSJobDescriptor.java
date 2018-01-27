9fbec70d2e57b4f86a09756b5c7653ed76758532
nicolas <nicolas.matteazzi@gmail.com>
2018-01-28 00:29:50 +0100

    Managing build of Jenkins jobs to toto-nodems-docker microservices

--- toto-devops-jenkins-init/src/main/java/com/imatz/toto/devops/jenkins/init/jenkins/JenkinsTotoMSJobDescriptor.java
+++ toto-devops-jenkins-init/src/main/java/com/imatz/toto/devops/jenkins/init/jenkins/JenkinsTotoMSJobDescriptor.java
@@ -86,6 +86,7 @@ public class JenkinsTotoMSJobDescriptor implements JenkinsJobDescriptor {
 		
 		String templateName = "jenkins-config-template.xml";
 		if (totoMsName_.startsWith("toto-nodems-")) templateName = "jenkins-nodems-template.xml";
+		if (totoMsName_.startsWith("toto-nodems-docker")) templateName = "jenkins-nodems-docker-template.xml";
 		
 		InputStream templateIS = this.getClass().getClassLoader().getResourceAsStream(templateName);
 		FileOutputStream templateCopyOS = null;
